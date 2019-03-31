ed14422c446f7fadb826b6685adc71bb3cf9fa59
U-MSI\Claire <cseiler@ufl.edu>
2019-03-31 16:16:43 -0400

    added user auth controller functions

--- client/js/controllers/loginController.js
+++ client/js/controllers/loginController.js
@@ -1,10 +1,20 @@
-angular.module('login').controller('LoginController', ['$scope', '$location','$window',
-  function($scope, $location, $window) {
+angular.module('login').controller('LoginController', ['$scope', 'Users', '$location','$window',
+  function($scope, $location, $window, Users) {
       $scope.form = {
-        username: null,
+        email: null,
+	username: null,
         password: null
     };
 
+    $scope.addUser = function() {
+     $scope.users.push($scope.newUser);
+     Users.create($scope.newUser).then(function() {
+       $scope.newUser = {};
+     }, function(err) {
+       console.log(err);
+     })
+    };
+    
     $scope.goLogin = function() {
 	var newLocation = location.href.substring(0, location.href.lastIndexOf("/"));
         $window.location.href = newLocation+'/dashboard.html';
