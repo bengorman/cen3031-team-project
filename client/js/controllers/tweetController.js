04e1050caa0601dd888a2434c1238a7adc98d1c2
bengorman <ben.gorman01@gmail.com>
2019-04-01 00:13:13 -0400

    got frontend trend by location search working

--- client/js/controllers/tweetController.js
+++ client/js/controllers/tweetController.js
@@ -1,20 +1,22 @@
 angular.module('tweets').controller('TweetController', ['$scope', 'Tweets', '$location','$window',
   function($scope, Tweets, $location, $window) {
-    $scope.search = "";
+    $scope.locationSearch = { name: "" };
+    $scope.keywordSearch = { query: "" }
     $scope.trends = [];
     $scope.tweets = [];
 
-    $scope.getTrends() = function() {
-      Tweets.searchLocation($scope.search).then(function(res) {
+    $scope.getTrends = function() {
+      console.log("SUBMIT");
+      Tweets.searchLocation($scope.locationSearch).then(function(res) {
         console.log(res.data);
-        trends = res.data;
+        $scope.trends = res.data;
       }, function(err) {
         console.log(err);
       });
     };
 
-    $scope.getTweets() = function() {
-      Tweets.searchKeyword($scope.search).then(function(res) {
+    $scope.getTweets = function() {
+      Tweets.searchKeyword($scope.keywordSearch).then(function(res) {
         console.log(res.data);
         tweets = res.data;
       }, function(err) {
@@ -22,4 +24,14 @@ angular.module('tweets').controller('TweetController', ['$scope', 'Tweets', '$lo
       });
     };
   }
-]);
+]).filter("nullToEnd", function () {
+    return function (array, key) {
+        var present = array.filter(function (item) {
+            return item[key];
+        });
+        var empty = array.filter(function (item) {
+            return !item[key]
+        });
+        return present.concat(empty);
+    };
+});
