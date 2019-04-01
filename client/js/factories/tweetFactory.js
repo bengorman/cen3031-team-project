a58251e94238b3fd02ffa8000ef19db900e1c8d4
bengorman <ben.gorman01@gmail.com>
2019-03-31 23:10:54 -0400

    fixed package.json, package-lock.json auto-updated, fixed tweetFactory.js

--- client/js/factories/tweetFactory.js
+++ client/js/factories/tweetFactory.js
@@ -1,11 +1,11 @@
 angular.module('tweets', []).factory('Tweets', function($http) {
   var methods = {
   	searchLocation: function(location) {
-  	  return $http.post('http://localhost:8080/api/users/', location);
+  	  return $http.post('http://localhost:8080/api/tweets/', location);
     },
 
     searchKeyword: function(keyword) {
-      return $http.put('http://localhost:8080/api/users/', keyword);
+      return $http.put('http://localhost:8080/api/tweets/', keyword);
     }
   };
 
