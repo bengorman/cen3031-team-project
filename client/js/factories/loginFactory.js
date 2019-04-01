dc6f16b035a055ad40748bbfe1f4d10c3c468fb8
bengorman <ben.gorman01@gmail.com>
2019-04-01 10:19:44 -0400

    changed URLs to heroku links

--- client/js/factories/loginFactory.js
+++ client/js/factories/loginFactory.js
@@ -1,11 +1,11 @@
 angular.module('login', []).factory('Users', function($http) {
   var methods = {
   	create: function(user) {
-  	  return $http.post('http://localhost:8080/api/users/', user);
+  	  return $http.post('https://twitter-analytics-dashboard.herokuapp.com/api/users/', user);
     },
 
     verify: function(user) {
-      return $http.put('http://localhost:8080/api/users/', user);
+      return $http.put('https://twitter-analytics-dashboard.herokuapp.com/api/users/', user);
     }
   };
 
