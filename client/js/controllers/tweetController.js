d2c587ab5376711c6497c009dde2c0e818b58a33
U-MSI\Claire <cseiler@ufl.edu>
2019-04-21 16:45:32 -0400

    testing graph

--- client/js/controllers/tweetController.js
+++ client/js/controllers/tweetController.js
@@ -1,7 +1,7 @@
 angular.module('tweets').controller('TweetController', ['$scope', 'Tweets', '$location','$window',/*'chart.js',*/
   function($scope, Tweets, $location, $window) {
     $scope.locationSearch = { name: "" };
-    $scope.keywordSearch = { query: "" };
+    $scope.keywordSearch = { query: "" }
     $scope.trends = [];
     $scope.tweets = [];
     $scope.unavailable = false;
@@ -11,6 +11,13 @@ angular.module('tweets').controller('TweetController', ['$scope', 'Tweets', '$lo
     $scope.labels1 = $scope.trends.name;
     $scope.series1 = ['Location Trends'];
     $scope.data1 = $scope.trends.tweet_volume;
+    
+        $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+    $scope.series = ['Series A', 'Series B'];
+    $scope.data = [
+      [65, 59, 80, 81, 56, 55, 40],
+      [28, 48, 40, 19, 86, 27, 90]
+    ];
 
     $scope.getTrends = function() {
       Tweets.searchLocation($scope.locationSearch).then(function(res) {
