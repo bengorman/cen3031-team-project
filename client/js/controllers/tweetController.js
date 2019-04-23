3d607a2e8917b789c905aea6bdee86b9142a4f85
bengorman <ben.gorman01@gmail.com>
2019-04-23 10:00:53 -0400

    debugging chart

--- client/js/controllers/tweetController.js
+++ client/js/controllers/tweetController.js
@@ -163,7 +163,7 @@ angular.module('tweets').controller('TweetController', ['$scope', 'Tweets', '$lo
         var handles = $scope.tweets.map(a => a.user.name);
         var favorites = $scope.tweets.map(b => b.favorite_count);
         var ctx = document.getElementById("keyword_like_bar").getContext('2d');
-        $scope.myChart1 = new Chart(ctx, {
+        $scope.myChart2 = new Chart(ctx, {
           type: 'bar',
           data: {
             labels: handles,
@@ -234,7 +234,7 @@ angular.module('tweets').controller('TweetController', ['$scope', 'Tweets', '$lo
         $scope.currentLocation.name = $scope.locationSearch.name;
         $scope.trends = res.data;
         $scope.trends.sort((a,b) => (a.tweet_volume > b.tweet_volume) ? -1 : ((b.tweet_volume > a.tweet_volume) ? 1 : 0));
-        delete $scope.myChart;
+        console.log($scope.myChart);
 	      $scope.generateTrendBarGraph();
       }, function(err) {
         console.log(err);
@@ -249,10 +249,8 @@ angular.module('tweets').controller('TweetController', ['$scope', 'Tweets', '$lo
         $scope.tweets = res.data.statuses;
         $scope.currentKeyword.query = $scope.keywordSearch.query;
         $scope.tweets.sort((a,b) => (a.retweet_count > b.retweet_count) ? -1 : ((b.retweet_count > a.retweet_count) ? 1 : 0));
-        delete $scope.myChart1;
         $scope.generateKeywordRetweetBarGraph();
         $scope.tweets.sort((a,b) => (a.favorite_count > b.favorite_count) ? -1 : ((b.favorite_count > a.favorite_count) ? 1 : 0));
-        delete $scope.myChart2;
         $scope.generateKeywordFavoriteBarGraph();
       }, function(err) {
         console.log(err);
